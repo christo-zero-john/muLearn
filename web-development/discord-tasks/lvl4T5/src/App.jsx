0f3ef59668df3a3ce59f1620473db2693704efb1
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-15 09:53:53 +0530

    Update App.jsx

--- web-development/discord-tasks/lvl4T5/src/App.jsx
+++ web-development/discord-tasks/lvl4T5/src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import AddItem from "./components/add-item";
+import "./styles.css";
+import "./defaults.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div>App</div>;
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    console.log("List: ", todoList);
+  }, [todoList]);
+
+  return (
+    <div className="en-Oxanium">
+      <AddItem list={todoList} setList={setTodoList} />
+    </div>
+  );
 }
 
 export default App;
