b7ed5c42aeaf446d837aaf9102b21f91100eaf7e
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-15 16:06:12 +0530

    Update App.jsx

--- web-development/discord-tasks/lvl4T5/src/App.jsx
+++ web-development/discord-tasks/lvl4T5/src/App.jsx
@@ -5,17 +5,30 @@ import "./defaults.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import { useEffect, useState } from "react";
+import ViewItems from "./components/view-items";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
+    let temp = JSON.parse(localStorage.getItem("todo-list"));
+    if (temp) {
+      setTodoList(temp);
+    } else {
+      localStorage.setItem("todo-list", JSON.stringify([]));
+    }
+  }, []);
+
+  useEffect(() => {
     console.log("List: ", todoList);
+    localStorage.setItem("todo-list", JSON.stringify(todoList));
   }, [todoList]);
 
   return (
-    <div className="en-Oxanium">
+    <div className="en-Oxanium bg-dark text-light hd-100 overflow-auto no-scrollbar">
+      <h1 className="text-center p-4">Simple ToDo React App</h1>
       <AddItem list={todoList} setList={setTodoList} />
+      <ViewItems list={todoList} setList={setTodoList} />
     </div>
   );
 }
